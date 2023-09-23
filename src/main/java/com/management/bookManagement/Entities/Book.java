e33b356593eeb0ebb606912e9d961e3a44dbb5e5
Russell <russell_soper@hotmail.co.uk>
2023-09-23 20:48:46 +0100

    Changed Book/Author relationship to manytomany

--- src/main/java/com/management/bookManagement/Entities/Book.java
+++ src/main/java/com/management/bookManagement/Entities/Book.java
@@ -2,27 +2,22 @@ package com.management.bookManagement.Entities;
 
 
 
-import jakarta.persistence.Basic;
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
 import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
-import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.hibernate.Hibernate;
-import org.springframework.context.annotation.Primary;
-
-import java.util.Objects;
+import java.util.List;
 
 @Entity
-@Builder
 @Getter
 @Setter
 @AllArgsConstructor
@@ -38,30 +33,36 @@ public class Book {
     @Column(name = "title")
     private String title;
 
-    @Column(name = "author")
-    private String author;
 
-    @Basic(fetch = FetchType.LAZY)
+    @ManyToMany(mappedBy = "books", cascade = CascadeType.ALL)
+    private List<Author> author;
+
     @Column(name = "read")
     private Boolean read;
 
-    @Basic(fetch = FetchType.LAZY)
     @Column(name = "owned")
     private Boolean owned;
 
     @Column(name = "genre")
     private String genre;
 
+
+    // The hashcode and equals methods were guided by this article
+    // https://thorben-janssen.com/ultimate-guide-to-implementing-equals-and-hashcode-with-hibernate/
     @Override
-    public boolean equals(Object o) {
-        if (this == o) return true;
-        if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        Book book = (Book) o;
-        return id != null && Objects.equals(id, book.id);
+    public int hashCode() {
+        return 13;
     }
 
     @Override
-    public int hashCode() {
-        return getClass().hashCode();
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Book other = (Book) obj;
+        return id != null && id.equals(other.getId());
     }
 }
