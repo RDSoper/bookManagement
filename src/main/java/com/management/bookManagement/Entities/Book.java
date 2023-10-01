c7bbc9a32204a38569737ff114798417f3e71489
Russell <russell_soper@hotmail.co.uk>
2023-10-01 13:40:15 +0100

    Add helper methods to book and author entities

--- src/main/java/com/management/bookManagement/Entities/Book.java
+++ src/main/java/com/management/bookManagement/Entities/Book.java
@@ -10,12 +10,14 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToMany;
 import jakarta.persistence.Table;
-import jakarta.persistence.Transient;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import java.util.List;
+
+import java.util.HashSet;
+import java.util.Set;
+
 
 @Entity
 @Getter
@@ -34,8 +36,8 @@ public class Book {
     private String title;
 
 
-    @ManyToMany(mappedBy = "books", cascade = CascadeType.ALL)
-    private List<Author> author;
+    @ManyToMany(mappedBy = "books", cascade = CascadeType.PERSIST)
+    private Set<Author> authors;
 
     @Column(name = "read")
     private Boolean read;
@@ -46,6 +48,19 @@ public class Book {
     @Column(name = "genre")
     private String genre;
 
+
+    public void addAuthor(Author author) {
+        if(this.authors == null ){
+            this.authors = new HashSet<>();
+        }
+        this.authors.add(author);
+        author.getBooks().add(this);
+    }
+
+    public void removeAuthor(Author author){
+        this.authors.remove(author);
+        author.getBooks().remove(this);
+    }
 
     // The hashcode and equals methods were guided by this article
     // https://thorben-janssen.com/ultimate-guide-to-implementing-equals-and-hashcode-with-hibernate/
