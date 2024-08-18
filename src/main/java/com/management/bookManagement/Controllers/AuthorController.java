1a2dc496c6a29a13c0f0da5a39a7531689c98f7c
Russell <russell_soper@hotmail.co.uk>
2024-08-18 14:36:28 +0100

    Add save author endpoint

--- src/main/java/com/management/bookManagement/Controllers/AuthorController.java
+++ src/main/java/com/management/bookManagement/Controllers/AuthorController.java
@@ -4,25 +4,35 @@ import com.management.bookManagement.DTO.AuthorDTO;
 import com.management.bookManagement.Entities.Author;
 import com.management.bookManagement.Services.AuthorService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.util.UriComponents;
+
 import static com.management.bookManagement.Utils.Mappers.mapAuthorToAuthorDTO;
 
+import java.net.URI;
+import java.net.http.HttpRequest;
 import java.util.ArrayList;
 import java.util.List;
 
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("author")
+@RequestMapping("/")
 public class AuthorController {
 
     AuthorService authorService;
 
-    @GetMapping("/all")
+    @GetMapping("authors")
     public List<AuthorDTO> getAuthors(){
         List<Author> authors =  authorService.getAuthors();
         List<AuthorDTO> authorDTOs = new ArrayList<>();
@@ -34,8 +44,14 @@ public class AuthorController {
         return authorDTOs;
     }
 
-    @GetMapping("/{id}")
-    public AuthorDTO getAuthor(@PathVariable Long id){
-        return mapAuthorToAuthorDTO(authorService.getAuthor(id));
+    @GetMapping("author/{id}")
+    public ResponseEntity<AuthorDTO> getAuthor(@PathVariable Long id){
+        return ResponseEntity.ok(mapAuthorToAuthorDTO(authorService.getAuthor(id)));
+    }
+
+    @PostMapping("author")
+    public ResponseEntity<Author> saveAuthor(@RequestBody Author author) {
+        Author newAuthor = authorService.saveAuthor(author);
+        return new ResponseEntity<>(newAuthor, HttpStatus.CREATED);
     }
 }
