5b80765dda8454cf69a6134186a1d83106c65b25
Russell <russell_soper@hotmail.co.uk>
2024-09-15 13:47:16 +0100

    Change repository to JpaRepository

--- src/main/java/com/management/bookManagement/Repositories/BookRepository.java
+++ src/main/java/com/management/bookManagement/Repositories/BookRepository.java
@@ -1,13 +1,13 @@
 package com.management.bookManagement.Repositories;
 
 import com.management.bookManagement.Entities.Book;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
 
 @Repository
-public interface BookRepository extends CrudRepository<Book, Long> {
+public interface BookRepository extends JpaRepository<Book, Long> {
     List<Book> findAllByOrderByTitleAsc();
     Book findByTitle(String title);
 }
