b7ab84f7b17d1c6a5d119ca51fb4bafaa7dd1418
Russell <russell_soper@hotmail.co.uk>
2024-09-15 14:03:14 +0100

    Add model mapper

--- src/main/java/com/management/bookManagement/Controllers/AuthorController.java
+++ src/main/java/com/management/bookManagement/Controllers/AuthorController.java
@@ -4,23 +4,17 @@ import com.management.bookManagement.DTO.AuthorDTO;
 import com.management.bookManagement.Entities.Author;
 import com.management.bookManagement.Services.AuthorService;
 import lombok.AllArgsConstructor;
-import org.springframework.http.HttpEntity;
+import org.modelmapper.ModelMapper;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.util.UriComponents;
 
-import static com.management.bookManagement.Utils.Mappers.mapAuthorToAuthorDTO;
-
-import java.net.URI;
-import java.net.http.HttpRequest;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -32,26 +26,29 @@ public class AuthorController {
 
     AuthorService authorService;
 
-    @GetMapping("authors")
-    public List<AuthorDTO> getAuthors(){
-        List<Author> authors =  authorService.getAuthors();
-        List<AuthorDTO> authorDTOs = new ArrayList<>();
+    ModelMapper modelMapper;
 
+    @GetMapping("authors")
+    public ResponseEntity<List<AuthorDTO>> getAuthors(){
+        List<Author> authors =  authorService.getAuthors();
+
+        List<AuthorDTO> authorDTOs = new ArrayList<>();
         for(Author author:authors){
-            authorDTOs.add(mapAuthorToAuthorDTO(author));
+            authorDTOs.add(modelMapper.map(author, AuthorDTO.class));
         }
 
-        return authorDTOs;
+        return ResponseEntity.ok(authorDTOs);
     }
 
     @GetMapping("author/{id}")
     public ResponseEntity<AuthorDTO> getAuthor(@PathVariable Long id){
-        return ResponseEntity.ok(mapAuthorToAuthorDTO(authorService.getAuthor(id)));
+        Author author = authorService.getAuthor(id);
+        return ResponseEntity.ok(modelMapper.map(author, AuthorDTO.class));
     }
 
     @PostMapping("author")
-    public ResponseEntity<Author> saveAuthor(@RequestBody Author author) {
+    public ResponseEntity<AuthorDTO> saveAuthor(@RequestBody Author author) {
         Author newAuthor = authorService.saveAuthor(author);
-        return new ResponseEntity<>(newAuthor, HttpStatus.CREATED);
+        return new ResponseEntity<>(modelMapper.map(newAuthor, AuthorDTO.class), HttpStatus.CREATED);
     }
 }
